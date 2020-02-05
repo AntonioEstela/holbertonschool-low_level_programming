2444adb08d736ce01c084bad278ab81998b96831
Antonio Estela <antonioestela73@gmail.com>
2020-02-05 20:28:02 +0000

    fixed task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <string.h>
-
-/* puts print the characters saved in the string variable */
-
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	char string[70] = "\"Programming is like building a multilingual puzzle";
