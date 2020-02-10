3131259b2dd93e99695800bfd2aec44b1a23b522
Antonio Estela <antonioestela73@gmail.com>
2020-02-10 10:02:45 -0500

    added task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * main - Print the alphabet
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter);
+	}
+	_putchar('\n');
+	return;
+}
