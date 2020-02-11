bf5f6b3d4ca8fa10f7308a3c2754e5fc957bbb1a
Antonio Estela <antonioestela73@gmail.com>
2020-02-10 19:41:42 -0500

    added task 100

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,52 @@
+#include "holberton.h"
+/**
+ * times_table - prints the 9 times table
+ * @void: no argument
+ */
+void print_times_table(int n)
+{
+	int num, num2, mul;
+
+	if (n > 15 || n < 0)
+	{
+		return;
+	}
+
+	for (num = 0; num <= n; num++)
+	{
+		for (num2 = 0; num2 <= n; num2++)
+		{
+			mul = (num * num2);
+			if (num2 == 0)
+			{
+				_putchar(mul + '0');
+			}
+			else if (mul <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(mul + '0');
+			}
+			else if (mul >= 10 && mul < 100)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(mul / 10 + '0');
+				_putchar(mul % 10 + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(mul / 100 + '0');
+				_putchar(mul / 10 % 10 + '0');
+				_putchar(mul % 10 + '0');
+
+			}
+		}
+		_putchar('\n');
+	}
+}
