933f3b06ac99f16da27b5979cafc11a6c6e5e3b5
Antonio Estela <antonioestela73@gmail.com>
2020-02-11 13:48:50 +0000

    fixed task 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,39 +1,36 @@
+#include <stdio.h>
 #include "holberton.h"
 /**
  * print_times_table - prints the n times table
+ * @n: integer
+ * @num: integer
+ * @num2: integer
+ * @mul: integer
  * @void: no argument
  */
-void print_times_table(int n)
+void print_times_table(int n, num, num2, mul)
 {
-	int num, num2, mul;
-
 	if (n > 15 || n < 0)
 	{
 		return;
 	}
-
 	for (num = 0; num <= n; num++)
 	{
 		for (num2 = 0; num2 <= n; num2++)
 		{
-			mul = (num * num2);
+			mul = (num *num2);
 			if (num2 == 0)
 			{
 				_putchar(mul + '0');
 			}
 			else if (mul <= 9)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
+				printf(",   ");
 				_putchar(mul + '0');
 			}
 			else if (mul >= 10 && mul < 100)
 			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
+				printf(",  ");
 				_putchar(mul / 10 + '0');
 				_putchar(mul % 10 + '0');
 			}
@@ -44,7 +41,6 @@ void print_times_table(int n)
 				_putchar(mul / 100 + '0');
 				_putchar(mul / 10 % 10 + '0');
 				_putchar(mul % 10 + '0');
-
 			}
 		}
 		_putchar('\n');
