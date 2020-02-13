b79e3833c82c3b38a3c80ac9ce18683426acaf0e
Antonio Estela <antonioestela73@gmail.com>
2020-02-13 22:41:54 +0000

    added task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+void print_square(int size)
+{
+	int x, y;
+
+	if (size > 0)
+	{
+
+	for (x = 1; x <= size; x++)
+	{
+
+		for(y = 1; y <= size; y++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+			
+	}
+	else
+	{
+		_putchar('\n');
+	}
+	
+	
+}
