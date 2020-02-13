cabb49ea5d63d42c81e2151563d49d49fa2e16da
Antonio Estela <antonioestela73@gmail.com>
2020-02-13 16:07:40 +0000

    updated task 3

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,9 +1,12 @@
 #include "holberton.h"
-
+/**
+ * print_numbers - funcion that print numbers from 0 - 9.
+ *
+*/
 void print_numbers(void)
 {
 	int num = 0;
-	
+
 	while (num < 10)
 	{
 		_putchar(num + '0');
