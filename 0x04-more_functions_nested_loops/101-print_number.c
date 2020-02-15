38b5540b77c03794d244b62ca5929b305b69b02c
Antonio Estela <antonioestela73@gmail.com>
2020-02-15 22:38:12 +0000

    added task 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+
+void print_number(int n)
+{
+	int abs = n * -1;
+
+	if (n >= 1000 && n <= 9999)
+	{
+		_putchar(n / 1000 + '0');
+		_putchar((n % 1000) / 100 + '0');
+		_putchar((n % 1000) / 10 + '0');
+		_putchar((n % 1000) % 10 + '0');
+	}	
+	else if (n >= 100 && n <= 999)
+	{
+		_putchar(n / 100 + '0');
+		_putchar((n % 100) / 10 + '0');
+		_putchar(n % 100 + '0');
+	}
+	else if (n <= 99 && n >= 10)
+	{
+		_putchar(n / 10 + '0');
+		_putchar(n % 10 + '0');
+	}
+
+	else if (n < 0)
+	{
+		_putchar('-');
+		_putchar(abs / 10+ '0');
+		_putchar(abs % 10 + '0');
+	}
+	else
+	{
+		_putchar(n +'0');
+	}
+				
+}
