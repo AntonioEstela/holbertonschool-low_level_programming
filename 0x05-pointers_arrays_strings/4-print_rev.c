7a1bee2c3b5bad07d677b0c30354eb03451c28a3
Antonio Estela <antonioestela73@gmail.com>
2020-02-18 22:19:38 +0000

    added task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_rev - function that prints a string, in reverse.
+ * @s: string
+ */
+void print_rev(char *s)
+{
+	int rev;
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	for (rev = count; rev >= 0; rev--)
+	{
+		_putchar(s[rev]);
+	}
+	_putchar('\n');
+}
