e87a15b526d3d5b7d3e882dd0bc4becc2b2e0726
Antonio Estela <antonioestela73@gmail.com>
2020-02-22 22:09:19 +0000

    fixed task 101

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,39 +1,35 @@
-#include "holberton.h"
+#include"holberton.h"
 /**
- * print_number -  function that prints an integer.
- * @n: integer
+* print_number - prints an integer.
+* @n: int n
+* Return: Always 0.
 */
 void print_number(int n)
 {
-	if (n >= 1000 && n <= 9999)
+	int i = n;
+	int j = 1;
+	
+	if (n < 0)
 	{
-		_putchar(n / 1000 + '0');
-		_putchar((n % 1000) / 100 + '0');
-		_putchar((n % 1000) / 10 + '0');
-		_putchar((n % 1000) % 10 + '0');
-	}
-	else if (n >= 100 && n <= 999)
-	{
-		_putchar(n / 100 + '0');
-		_putchar((n % 100) / 10 + '0');
-		_putchar(n % 100 + '0');
-	}
-	else if (n <= 99 && n >= 10)
-	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	}
-
-	else if (n < 0)
-	{
-		n = n * -1;
-
+		j = -1;
 		_putchar('-');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+	}
+	while (i > 9 || i < -9)
+	{
+		i = i / 10;
+		j = j * 10;
+	}
+	while (j > 9 || j < -9)
+	{
+		_putchar((n / j) % 10 + '0');
+		j = j / 10;
+	}
+	if (n < 0)
+	{
+		_putchar((n % 10)  * -1 + '0');
 	}
 	else
 	{
-		_putchar(n + '0');
+		_putchar(n % 10 + '0');
 	}
 }
