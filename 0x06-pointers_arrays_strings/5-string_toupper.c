def06b3cde4753efa4c5a2360e0b720ec731cd9f
Antonio Estela <antonioestela73@gmail.com>
2020-02-22 17:46:25 +0000

    added task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+char *string_toupper(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			str[i] = str[i] - 32;
+		}
+		i++;
+	}
+	return (str);
+}
