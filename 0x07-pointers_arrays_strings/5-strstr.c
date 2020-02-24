183847871c34d827f1d294be5aac47069279e02c
Antonio Estela <antonioestela73@gmail.com>
2020-02-24 23:54:53 +0000

    fixed task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,19 +8,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	int i, j;
+	int i, j, p;
 
 	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; needle[j] != '\0'; i++)
+		for (p = i, j = 0; needle[j] != '\0'; j++, p++)
 		{
-			if (haystack[i] == needle[j])
+			if (haystack[p] != needle[j] || haystack[p] == '\0')
 			{
-				return (haystack + i);
+				break;
 			}
 		}
-		if (haystack[i] == needle[j])
-		{
+		if (needle[j] == '\0')
+		{	
 			return (haystack + i);
 		}
 	}
