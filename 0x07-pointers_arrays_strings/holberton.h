d68372d6ab981427df50c382d9cae8263899e50e
Antonio Estela <antonioestela73@gmail.com>
2020-02-25 17:49:58 +0000

    added task 7

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -11,5 +11,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void set_string(char **s, char *to);
+void print_chessboard(char (*a)[8]);
+int _putchar(char c);
 
 #endif /* HOLBERTON_H */
