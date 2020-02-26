7caa61a2695ea15eca453502d5917ee16d4ed543
Antonio Estela <antonioestela73@gmail.com>
2020-02-26 16:55:11 +0000

    added holberton.h

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+#define NULL 0
+
+#include <stdio.h>
+
+int _putchar(char c);
+int factorial(int n);
+void _puts_recursion(char *s);
+
+#endif /* HOLBERTON_H */
