ef27f2d10ccb3d8be5638e26d43d8242eee3d1d3
Antonio Estela <antonioestela73@gmail.com>
2020-02-26 20:22:53 +0000

    added task 4

--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -8,5 +8,6 @@ int factorial(int n);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int _pow_recursion(int x, int y);
 
 #endif /* HOLBERTON_H */
