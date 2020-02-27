e893b6be47521d794ff65062955c09cc75811c19
Antonio Estela <antonioestela73@gmail.com>
2020-02-27 02:55:19 +0000

    fixed task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,31 +1,40 @@
 #include "holberton.h"
-
+/**
+ * _operation - function that checks for a prime number
+ * @num: integer.
+ * @i: integer.
+ * Return: 1 if prime; 0 if not prime.
+ */
 int _operation(int num, int i)
 {
-    if (i == 1)
-    {
-        return 1;
-    }
-    else
-    {
-       if (num % i == 0)
-       {
-         return 0;
-       }
-       else
-       {
-         return _operation(num, i - 1);
-       }       
-    }
+	if (i == 1)
+	{
+	return (1);
+	}
+	else
+	{
+		if (num % i == 0)
+		{
+			return (0);
+		}
+		else
+		{
+			return (_operation(num, i - 1));
+		}
+	}
 }
-
+/**
+ * is_prime_number - function that checks for a prime number
+ * @n: integer.
+ * Return: 1 if prime; 0 if not prime.
+ */
 int is_prime_number(int n)
 {
 	if (n < 2)
 	{
 		return (0);
 	}
-	
-	return(_operation(n, n / 2));
+
+	return (_operation(n, n / 2));
 
 }
