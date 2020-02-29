74d51577e303bef03e38ce724b9e443f87680dd9
Antonio Estela <antonioestela73@gmail.com>
2020-02-29 22:58:24 +0000

    fixed task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,18 +10,19 @@ int main(int argc, char *argv[])
 {
 	int i, result = 0;
 
+	if (argc < 1)
+		return (0);
+
 	for (i = 1; i < argc; i++)
 	{
-		if (atoi(argv[i]))
+		if (!atoi(argv[i]))
 		{
-			result += atoi(argv[i]);
-		}
-		else
-		{
-			printf("Error\n");
+			printf("%s\n", "Error");
 			return (1);
 		}
+		result += atoi(argv[i]);
 	}
 	printf("%d\n", result);
+
 	return (0);
 }
