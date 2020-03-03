b8824589bbf99dfa52def56655a01ce73a5b51a6
Antonio Estela <antonioestela73@gmail.com>
2020-03-03 21:03:33 +0000

    added task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+/**
+ * _strlen - function that returns the length of a string..
+ * @s: string
+ * Return: the value of the variable count.
+ */
+int _strlen(char *s)
+{
+	int count = 0;
+
+	while (s[count] != '\0')
+	{
+		count++;
+	}
+	return (count);
+}
+/**
+ * _strdup - function that returns a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: string.
+ * Return: Returns NULL if str = NULL or if insufficient memory was available
+ */
+char *_strdup(char *str)
+{
+	int i;
+	char *dest;
+
+	dest = malloc(sizeof(str) * _strlen(str));
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (i = 0; *(str + i) != '\0'; i++)
+		{
+			*(dest + i) = *(str + i);
+		}
+		return (dest);
+	}
+	free(dest);
+}
