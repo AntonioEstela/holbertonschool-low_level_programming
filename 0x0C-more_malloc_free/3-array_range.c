66126e78dfb60c2eb1a86453429126128eee0a4a
Antonio Estela <antonioestela73@gmail.com>
2020-03-05 22:14:44 +0000

    fixed task 5

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,21 +7,23 @@
  */
 int *array_range(int min, int max)
 {
-	int *arr, i;
+	int *arr;
+	int i, j;
 
+	j = max - min;
 	if (min > max)
 	{
 		return (NULL);
 	}
-
-	arr = malloc(sizeof(int) * ((max -  min) + 1));
+	arr = malloc(sizeof(int) * (j + 1));
 	if (arr == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i <= (max - min); i++)
+	for (i = 0; i <= (j); i++, min++)
 	{
-		*(arr + i) = i;
+		*(arr + i) = min;
 	}
 	return (arr);
+
 }
