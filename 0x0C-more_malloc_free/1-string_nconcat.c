c1aa41234063f773861be90a6d84fcf81789b8c6
Antonio Estela <antonioestela73@gmail.com>
2020-03-05 19:06:02 +0000

    fixed task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -7,5 +7,57 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	unsigned int i, j, k, l;
+	int size;
+	char *dest;
 
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	/* code to find the length of s1 and s2 */
+	i = 0;
+	while (*(s1 + i) != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (*(s2 + j) != '\0')
+	{
+		j++;
+	}
+	/* code for concatenates two strings. */
+	size = i + j;
+
+	dest = malloc(sizeof(char) * size);
+	if (dest == NULL)
+	{
+		return (NULL);
+	}
+	if (n >= j)
+	{
+		for (l = 0; l < n; l++)
+		{
+			*(dest + l) = *(s2 + l);
+		}
+		*(dest + l) = '\0';
+	}
+	else
+	{	
+		for (k= 0; k < i; k++)
+		{
+			*(dest + k) = *(s1 + k);
+		}
+		for (l = 0; l < n; l++)
+		{
+			*(dest + (k + l)) = *(s2 + l);
+		}
+		*(dest + (k + l)) = '\0';
+	}
+	return (dest);
 }
