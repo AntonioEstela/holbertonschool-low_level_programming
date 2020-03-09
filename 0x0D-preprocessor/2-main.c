c71bdc07f3090f6f5735ab127919df549cb874dd
Antonio Estela <antonioestela73@gmail.com>
2020-03-09 17:07:52 +0000

    fixed all task with betty style

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints the name of the file it was compiled from.
+ * Return: nothing.
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,1 +1,6 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
 #define ABS(x) ((x) > 0 ? (x) : (x) * -1)
+
+#endif
