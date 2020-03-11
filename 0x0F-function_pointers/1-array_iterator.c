f0f581f39f14d280980ee9ac53976229da5c9728
Antonio Estela <antonioestela73@gmail.com>
2020-03-11 19:29:42 +0000

    added task 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - function that executes a function
+ * given as a parameter on each element of an array.
+ * @array: array to iterate.
+ * @size: size of the array.
+ * @action: function.
+ * Return: nothing.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i, j;
+
+	j = size;
+	if (array == NULL || *action == NULL)
+	{
+		return;
+	}
+	for (i = 0; i < j; i++)
+	{
+		(*action)(*array + i);
+	}
+}
