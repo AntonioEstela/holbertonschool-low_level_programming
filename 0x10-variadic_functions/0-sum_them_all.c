4750c1571d1dcb56c456d25e55d72461a02ae93b
Antonio Estela <antonioestela73@gmail.com>
2020-03-13 15:45:19 +0000

    added task 0

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * sum_them_all - function that returns the sum of all its parameters.
+ * @n: number to add.
+ * Return: 0 if n is NULL or the result of sum.
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i, sum = 0;
+
+	va_start(list, n);
+
+	if (n == NULL)
+	{
+		return (0);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, unsigned int);
+	}
+	va_end(list);
+
+	return (sum);
+}
