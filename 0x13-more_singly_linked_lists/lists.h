2265d704e1b06b730833aa4058bb62171649b68f
Antonio Estela <antonioestela73@gmail.com>
2020-03-23 22:18:36 +0000

    Fixed header file with Betty code style

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -14,8 +13,8 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
