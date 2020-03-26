d0338f79742b6af4e8c01ac3fc03962d0ff3034e
Antonio Estela <antonioestela73@gmail.com>
2020-03-26 23:46:55 +0000

    Fixed task 1 (again) :c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,20 +7,15 @@
 void binary_printing(unsigned int n)
 {
 	if (n == 0)
-	{
 		return;
-	}
 
 	binary_printing(n >> 1);
 
-	if (n & 1)
-	{
+	if ((n & 1) == 1)
 		_putchar('1');
-	}
-	if (!(n & 1))
-	{
+
+	if ((n & 1) == 0)
 		_putchar('0');
-	}
 }
 
 /**
