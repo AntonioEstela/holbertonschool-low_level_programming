452648a0e210d85c9820b15d95940b076a18cdec
Antonio Estela <antonioestela73@gmail.com>
2020-03-26 19:54:14 +0000

    Finally fixed task 0 (Yeeeah)

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,17 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int num = 0;
 	unsigned int i;
 
-	if (b == '\0')
+	if (b == NULL)
 	{
-		return (1);
+		return (0);
 	}
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
+		}
 
 		num = (num * 2) + (b[i] - '0');
 	}
