977bfe993b9370e5a430fba38c364eb513815c1d
Antonio Estela <antonioestela73@gmail.com>
2020-03-27 00:43:41 +0000

    Added task 3 :3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * set_bit - function that sets the value of a bit to 1 at a given index.
+ * @n: number to get the bit in the index position.
+ * @index: position used to get the bit.
+ * Return: 1 if it worked, or -1 if an error occurred.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int bits = (sizeof(unsigned int) * 8);
+
+	if (index > bits)
+	{
+		return (-1);
+	}
+
+	*n = *n | (1 << index);
+	return (1);
+}
