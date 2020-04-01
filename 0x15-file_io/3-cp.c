a2a34965794bd991c59ba50835864058a5c9eb6c
Antonio Estela <antonioestela73@gmail.com>
2020-04-01 01:51:27 +0000

    Trying to fix the task 3 😪

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	if (num_char <= -1)
+	if (num_char == -1 || num_char != rd1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
