f7527683000154cf12704149e83179eae2a62af2
Antonio Estela <antonioestela73@gmail.com>
2020-04-01 02:10:05 +0000

    Trying to fix this file :c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,23 +12,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28);
-		exit(97);
+		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 28), exit(97);
 	}
 	file1 = open(argv[1], O_RDONLY);
 	if (file1 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]),
 		exit(97);
 	}
 	file2 = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 	if (file2 == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
-	do
-	{
+	do {
 		rd1 = read(file1, buffer, 1024);
 		num_char = write(file2, buffer, rd1);
 	} while (rd1 == 1024);
@@ -39,18 +36,15 @@ int main(int argc, char *argv[])
 	}
 	if (num_char == -1 || num_char != rd1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (close(file1) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1), exit(100);
 	}
 	if (close(file2) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2);
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file2), exit(100);
 	}
 	return (0);
 }
