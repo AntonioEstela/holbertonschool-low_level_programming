c10bcab83cd3d49bb269a44a5019168d238d3359
Antonio Estela <antonioestela73@gmail.com>
2020-05-01 21:13:36 +0000

    Added some description

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,8 +1,8 @@
 #include "lists.h"
 /**
- * 
- * 
- * 
+ * dlistint_len - Function that returns the number of nodes in a DLL.
+ * @h: Pointer to the head of the double linked list.
+ * Return: The number of nodes.
  */
 size_t dlistint_len(const dlistint_t *h)
 {
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -20,6 +20,8 @@ typedef struct dlistint_s
 	struct dlistint_s *next;
 } dlistint_t;
 
+/* List of the functions used in the project */
 size_t print_dlistint(const dlistint_t *h);
+size_t dlistint_len(const dlistint_t *h);
 
 #endif /* LISTS_H */
