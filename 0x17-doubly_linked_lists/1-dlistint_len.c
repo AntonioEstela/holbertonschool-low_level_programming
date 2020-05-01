e3e88397097594660f1b8b9cf6e40123b4784df4
Antonio Estela <antonioestela73@gmail.com>
2020-05-01 21:28:06 +0000

    Fixed betty code style and added more funcitons in lislts.h

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -7,7 +7,7 @@
 size_t dlistint_len(const dlistint_t *h)
 {
 	size_t number_of_elements;
-	dlistint_t *list = (dlistint_t*)h;
+	dlistint_t *list = (dlistint_t *)h;
 
 	for (number_of_elements = 0; list != NULL; number_of_elements++)
 	{
