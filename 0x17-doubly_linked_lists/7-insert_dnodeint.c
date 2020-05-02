49309371f474a247c8d4a4ba96c2af2720269e5e
Antonio Estela <antonioestela73@gmail.com>
2020-05-02 18:32:04 +0000

    Fix task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -33,7 +33,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-	if (idx == '\0')
+	if (idx == 0)
 	{
 		new = add_dnodeint(h, n);
 		return (new);
