bc4b1664d9f99e1087026689c96d2411ea5d084f
Antonio Estela <antonioestela73@gmail.com>
2020-05-02 18:17:05 +0000

    Fix task 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,22 @@
 #include "lists.h"
 /**
+ * dlistint_len - Function that returns the number of nodes in a DLL.
+ * @h: Pointer to the head of the double linked list.
+ * Return: The number of nodes.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t number_of_elements;
+	dlistint_t *list = (dlistint_t *)h;
+
+	for (number_of_elements = 0; list != NULL; number_of_elements++)
+	{
+		list = list->next;
+	}
+
+	return (number_of_elements);
+}
+/**
  * insert_dnodeint_at_index - Function that inserts a new node at idx position.
  * @h: Pointer to the head of the double linked list.
  * @idx: is the index of the list where the new node should be added.
@@ -8,16 +25,16 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new;
 	dlistint_t *list = (*h);
+	dlistint_t *new = NULL;
+	dlistint_t *end = NULL;
+	size_t len = dlistint_len(*h);
 	unsigned int i = 0;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->n = n;
-
 	if (idx == '\0')
 	{
 		new->prev = NULL;
@@ -25,7 +42,11 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		(*h) = new;
 		return (new);
 	}
-
+	if (idx > len)
+	{
+		end = add_dnodeint_end(h, n);
+		return (end);
+	}
 	while (list != NULL)
 	{
 		if (i == (idx - 1))
