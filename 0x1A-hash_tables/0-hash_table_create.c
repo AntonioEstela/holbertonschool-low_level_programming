2fb06996b78e4cfc9a193fd6975a461b4cdaeb0c
Antonio Estela <antonioestela73@gmail.com>
2020-05-29 17:23:56 +0000

    Fixed task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,6 +19,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	}
 
 	new_hash_table->size = size;
+
 	new_hash_table->array = calloc(size, sizeof(hash_node_t *));
 	if (new_hash_table->array == NULL)
 	{
