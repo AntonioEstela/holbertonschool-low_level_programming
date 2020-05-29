99bd0e600c1b12a6d45e02a11d2d8fca6aef71eb
Antonio Estela <antonioestela73@gmail.com>
2020-05-29 17:14:46 +0000

    Fixed task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,8 +9,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_hash_table;
 	unsigned int i;
 
-	new_hash_table = malloc(sizeof(new_hash_table));
-	if (new_hash_table == NULL || !size)
+	new_hash_table = malloc(sizeof(new_hash_table) * 1);
+	if (new_hash_table == NULL || size == '\0')
 		return (NULL);
 
 	new_hash_table->size = size;
