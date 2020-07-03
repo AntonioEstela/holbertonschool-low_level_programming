28bcfd4aab1dd780ecfbef32bd900ce6210822d1
Antonio Estela <antonioestela73@gmail.com>
2020-07-03 16:00:39 -0500

    Added task 5

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Island Perimeter module """
+
+
+def island_perimeter(gird):
+    """Function that returns the perimeter of the island described in grid"""
+
+    Perimeter = 0
+
+    for y in range(len(gird)):
+        for x in range(len(gird[y])):
+            if gird[y][x] == 1:
+                print('Lado')
+                Perimeter += 4
+
+                try:
+                    if gird[y][x - 1] == 1:
+                        Perimeter -= 1
+
+                    if gird[y][x + 1] == 1:
+                        Perimeter -= 1
+
+                    if gird[y - 1][x] == 1:
+                        Perimeter -= 1
+
+                    if gird[y + 1][x] == 1:
+                        Perimeter -= 1
+
+                except Exception:
+                    pass
+
+    return Perimeter
